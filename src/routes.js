a4cb0e452a21da64be82efd3e4b802b58dcf7179
MCarvalho <178carvalho@gmail.com>
2019-10-21 05:48:57 -0300

    Finalizando desafio 02

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import StudentController from './app/controllers/StudentController';
+import SessionContoller from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.post('/sessions', SessionContoller.store);
+
+routes.use(authMiddleware);
+
+routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
